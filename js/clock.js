5e1ad80f58098d7edf8cae6969548200430818f9
nofearrecords <nofearrecords@daum.net>
2018-02-28 18:38:13 +0900

    Update : loading page to clock

---
+++ js/clock.js
@@ -0,0 +1,20 @@
+function startTime() {
+
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var ms = today.getMilliseconds();
+    
+    m = checkTime(m);
+    s = checkTime(s);
+    ms = checkTime(ms);
+    
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + ":" + ms;
+    var t = setTimeout(startTime, 30);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
