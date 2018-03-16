95bc4232c95d3baa227ed1da95c5cf72ac0edfd2
nofearrecords <nofearrecords@daum.net>
2018-03-17 02:41:35 +0900

    Update

--- js/clock.js
+++ js/clock.js
@@ -1,22 +1,25 @@
 function startTime() {
 
+    var timer;
+
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
-    var ms = today.getMilliseconds();
-    
+    var ms = today.getMilliseconds();   
+
+    h = checkTime(h); 
     m = checkTime(m);
     s = checkTime(s);
     ms = checkTime(ms);
     
     try {
 	document.getElementById('clock').innerHTML = h + ":" + m + ":" + s + ":" + ms;
+	timer = setTimeout(startTime, 30);
     }
     catch(e) {
-	
+	clearTimeout(timer);
     }
-    var t = setTimeout(startTime, 30);
 }
 
 function checkTime(i) {
