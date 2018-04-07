ce42ef8b1b141488560d9e75bbfdd739cacaaf0f
Jeonghan Seok <rudebono@gmail.com>
2018-04-08 04:01:43 +0900

    Update : menu

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+﻿activate = 0
+
+function myFunctionMenu() {
+
+    //on
+    if(activate == 0) {
+        document.getElementById('nav_deactivate').setAttribute('id', 'nav_activate');
+        document.getElementById('bar1_deactivate').setAttribute('id', 'bar1_activate');
+        document.getElementById('bar2_deactivate').setAttribute('id', 'bar2_activate');
+        document.getElementById('bar3_deactivate').setAttribute('id', 'bar3_activate');
+        document.getElementById('nav_content').style.visibility = "visible";
+        activate = 1;
+    } else {
+        document.getElementById('nav_activate').setAttribute('id', 'nav_deactivate');
+        document.getElementById('bar1_activate').setAttribute('id', 'bar1_deactivate');
+        document.getElementById('bar2_activate').setAttribute('id', 'bar2_deactivate');
+        document.getElementById('bar3_activate').setAttribute('id', 'bar3_deactivate');
+        document.getElementById('nav_content').style.visibility = "hidden";
+        activate = 0;
+    }
+
+}
